15fea56f40325ade94dc082d785d9fb282772ed4
ssewabuga <benardleno78@gmail.com>
2025-08-27 16:18:45 +0300

    'fixing'

--- backend/settings.py
+++ backend/settings.py
@@ -101,27 +101,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-DATABASE_URL = os.getenv("DATABASE_URL") 
-
-if DATABASE_URL:
- DATABASES = {
-    'default': dj_database_url.config(
+DATABASES = {
+    "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,   
-        ssl_require=True    
+        conn_max_age=600,
+        ssl_require=True
     )
-}
-else:
- DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'smsdb'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-    }
-}
+}  
+
+
+#DATABASES = {
+    
+   #     'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'smsdb',
+   #     'USER':'postgres',
+    #    'PASSWORD': 'ben@rd1123',
+    #    'HOST':'localhost',
+    #    'PORT': '5432',
+    
+#}
 
 
 

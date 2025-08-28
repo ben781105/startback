0c895f8e514edb0093eea7a07c51af509f65432e
ssewabuga <benardleno78@gmail.com>
2025-08-28 22:46:44 +0300

    africastaliking api key to safety

--- sms/tasks.py
+++ sms/tasks.py
@@ -1,9 +1,13 @@
 from celery import shared_task
 import africastalking
+import os
 
 @shared_task
 def send_bulk_sms_task(message,recipients):
-    africastalking.initialize('sandbox','atsk_b69111d08e6995fb9fc31cca075f47c5bd3221eff28fe32c67baf8ca0337101cc5b6dc15')
+    africastalking.initialize(
+        os.getenv("AFRICASTALKING_USERNAME"),
+        os.getenv("AFRICASTALKING_API_KEY")
+    )
     sms = africastalking.SMS
-    response= sms.send(message,recipients)
+    response = sms.send(message, recipients)
     return response
